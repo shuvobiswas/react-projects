8212b660bf2359077ca766aacab4d9923ebfc1bf
Shuvo Biswas <shuvobs60@gmail.com>
2026-07-07 19:24:39 +0600

    Add ShoppingList component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,10 @@
 import OTPGenerator from "./Components/OTPGenerator";
-
+import  ShoppingList  from "./Components/ShoppingList";
 function App() {
   return (
     <div>
       <OTPGenerator />
+      <ShoppingList />
     </div>
   );
 }
